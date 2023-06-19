4e879f795cd79e0d5c4bb3ef2f3c4e817b245582
Kalinina Oksana <ksan4ik92@gmail.com>
2023-06-20 00:47:58 +0300

    feat: fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathFile = path.join(__dirname, "files", "fileToRemove.txt");
+
+  fs.access(pathFile, fs.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+    fs.unlink(pathFile, (err) => {
+      if (err) throw err;
+    });
+  });
 };
 
 await remove();
