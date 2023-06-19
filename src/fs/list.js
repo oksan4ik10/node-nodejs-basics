711f898a14439ccdfca3a195d8006dd7449782f9
Kalinina Oksana <ksan4ik92@gmail.com>
2023-06-20 00:53:47 +0300

    feat: fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathFile = path.join(__dirname, "files");
+  fs.readdir(pathFile, (err, files) => {
+    if (err) throw new Error("FS operation failed");
+    else {
+      console.log(...files);
+    }
+  });
 };
 
 await list();
