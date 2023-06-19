42356e2df5607c0ca7597769731b95eda0c03285
Kalinina Oksana <ksan4ik92@gmail.com>
2023-06-20 01:09:07 +0300

    feat: cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+  process.env.RSS_name1 = "2323";
+  process.env.RSS_name2 = "23444423";
+  process.env.RSS = "23243";
+  const env = process.env;
+
+  let res = "";
+
+  for (const key in env) {
+    if (key.slice(0, 4) === "RSS_") {
+      res = res + `${key}=${env[key]}; `;
+    }
+  }
+  console.log(res.trim());
 };
 
 parseEnv();
