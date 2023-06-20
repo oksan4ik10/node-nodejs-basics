3bff933f441fd75f8aa5c5fe528ce881ad3c3a87
Kalinina Oksana <ksan4ik92@gmail.com>
2023-06-20 23:02:17 +0300

    feat:streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathFile = path.join(__dirname, "files", "fileToRead.txt");
+
+  const stream = new fs.ReadStream(pathFile, { encoding: "utf-8" });
+
+  stream.on("readable", function () {
+    const data = stream.read();
+    if (data) process.stdout.write(data);
+  });
 };
 
 await read();
