38e6997b8451fb0a20036079e36eeb8c952da789
Kalinina Oksana <ksan4ik92@gmail.com>
2023-06-20 23:16:45 +0300

    feat:streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathFile = path.join(__dirname, "files", "fileToWrite.txt");
+  const writableStream = fs.createWriteStream(pathFile);
+
+  process.stdin.setEncoding("utf8");
+
+  process.stdin.on("readable", () => {
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+      writableStream.write(chunk);
+    }
+  });
 };
 
 await write();
