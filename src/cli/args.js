b08c093a6073a099113c9ed94e5f869aea9f42ba
Kalinina Oksana <ksan4ik92@gmail.com>
2023-06-20 22:32:03 +0300

    feat: cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,12 @@
-const parseArgs = () => {};
+const parseArgs = () => {
+  const args = process.argv;
+  let res = "";
+  args.forEach((item, index) => {
+    if (item.slice(0, 2) === "--") {
+      res += `${item} is ${args[index + 1]}, `;
+    }
+  });
+  console.log(res.slice(0, res.length - 2));
+};
 
 parseArgs();
