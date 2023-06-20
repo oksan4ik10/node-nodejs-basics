93907a9dc81a0a7cbad2c8dc7a797271b81f90ed
Kalinina Oksana <ksan4ik92@gmail.com>
2023-06-20 22:52:36 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import crypto from "crypto";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathFile = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  const fd = fs.createReadStream(pathFile);
+  const hash = crypto.createHash("sha256");
+  hash.setEncoding("hex");
+
+  fd.on("end", function () {
+    hash.end();
+    console.log(hash.read());
+  });
+
+  fd.pipe(hash);
 };
 
 await calculateHash();
